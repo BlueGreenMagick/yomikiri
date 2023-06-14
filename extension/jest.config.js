6986317f6d6a4ae0899766bca8678f0aba797b0e
Yoonchae Lee <bluegreenmagick@gmail.com>
2023-06-14 19:46:58 +0900

    develop: map paths for jest

--- extension/jest.config.js
+++ extension/jest.config.js
@@ -2,4 +2,10 @@ module.exports = {
   transform: {
     "^.+\\.(t|j)sx?$": "@swc/jest",
   },
+  moduleNameMapper: {
+    "^~/(.*)$": "./$1",
+    "^@icons/(.*)$": "assets/icons/$1",
+    "^@platform$": "platform/desktop/index",
+    "^@platform/(.*)$": "platform/desktop/$1",
+  },
 };
