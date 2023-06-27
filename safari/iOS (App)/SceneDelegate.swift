0f39c50bb6e28953fbf631d19944fc9324971215
Yoonchae Lee <bluegreenmagick@gmail.com>
2023-06-27 22:17:14 +0900

    fix: ios app sometimes failing to retrieve Anki clipboard data

--- safari/iOS (App)/SceneDelegate.swift
+++ safari/iOS (App)/SceneDelegate.swift
@@ -17,34 +17,45 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        if let url = connectionOptions.urlContexts.first?.url {
+            Task {
+                await handleXCallbackUrl(url: url)
+            }
+        }
         guard let _ = (scene as? UIWindowScene) else { return }
+        
     }
     
     func scene(
         _ scene: UIScene,
         openURLContexts urlContexts: Set<UIOpenURLContext>
     ) {
+        guard let url = urlContexts.first?.url else {
+            return
+        }
         Task {
-            guard let url = urlContexts.first?.url else {
-                return
-            }
-            // let openingProcess = urlContexts.first?.options.sourceApplication
-            os_log(.default, "Opened url (scene): %{public}s", url.absoluteString)
-            do {
-                switch(url.path) {
-                case "/infoForAdding":
-                    try await storeAnkiData()
-                    let yomikiriRedirectUrl = URL(string: "http://yomikiri-redirect.bluegreenmagick.com")!
-                    await UIApplication.shared.open(yomikiriRedirectUrl)
-                default:
-                    os_log(.error, "No x-callback-url action found")
-                }
-            } catch {
-                os_log(.error, "ERROR: %s", error.localizedDescription)
-            }
+            await handleXCallbackUrl(url: url)
         }
     }
 }
+
+func handleXCallbackUrl(url: URL) async {
+    // let openingProcess = urlContexts.first?.options.sourceApplication
+    os_log(.default, "Opened url (scene): %{public}s", url.absoluteString)
+    do {
+        switch(url.path) {
+        case "/infoForAdding":
+            try await storeAnkiData()
+            let yomikiriRedirectUrl = URL(string: "http://yomikiri-redirect.bluegreenmagick.com")!
+            await UIApplication.shared.open(yomikiriRedirectUrl)
+        default:
+            os_log(.error, "No x-callback-url action found")
+        }
+    } catch {
+        os_log(.error, "ERROR: %s", error.localizedDescription)
+    }
+}
+
 
 // store anki info data into UserDefaults
 func storeAnkiData() async throws {
@@ -52,11 +63,24 @@ func storeAnkiData() async throws {
     guard let sharedDefault = UserDefaults(suiteName: "group.com.bluegreenmagick.yomikiri") else {
         throw "Could not retrieve UserDefaults"
     }
-    try await Task.sleep(nanoseconds: 10 * 1000)
-    guard let data = UIPasteboard.general.data(forPasteboardType: PASTEBOARD_TYPE) else {
+    var tries = 0;
+    var data: Data?
+    // sometimes takes some time for clipboard data to appear
+    while (tries < 25) {
+        if let d = UIPasteboard.general.data(forPasteboardType: PASTEBOARD_TYPE) {
+            // clear clipboard
+            UIPasteboard.general.setData(Data(), forPasteboardType: PASTEBOARD_TYPE)
+            data = d
+            break;
+        }
+        try await Task.sleep(nanoseconds: 10 * 1000)
+        tries += 1
+    }
+    if (tries == 25){
         throw "Anki data not found in clipboard"
     }
+    
     // clear clipboard
     UIPasteboard.general.setData(Data(), forPasteboardType: PASTEBOARD_TYPE)
-    sharedDefault.set(data, forKey: "ankiInfo")
+    sharedDefault.set(data!, forKey: "ankiInfo")
 }
