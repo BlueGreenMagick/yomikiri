1a1765c3867185cd531a66b355c2d1bfdc8b97dc
Yoonchae Lee <bluegreenmagick@gmail.com>
2023-07-07 21:01:23 +0900

    fix: disallow selecting on non-japanese tokens in popup

--- extension/src/popup/TokensView.svelte
+++ extension/src/popup/TokensView.svelte
@@ -1,18 +1,32 @@
 <script lang="ts">
-  import { RubyString } from "~/japanese";
+  import { RubyString, containsJapaneseContent } from "~/japanese";
   import type { Token } from "~/tokenizer";
 
   export let tokens: Token[];
   export let selectedIdx: number = 0;
+
+  let invalidTokens: boolean[];
+
+  function invalidToken(token: Token) {
+    return !containsJapaneseContent(token.text);
+  }
+
+  $: invalidTokens = tokens.map(invalidToken);
 </script>
 
 <div>
   {#each tokens as token, idx}
-    <div class="token" class:selected={selectedIdx === idx}>
+    <div
+      class="token"
+      class:selected={selectedIdx === idx}
+      class:invalid={invalidTokens[idx]}
+    >
       <a
         href={"#"}
         on:click={() => {
-          selectedIdx = idx;
+          if (!invalidTokens[idx]) {
+            selectedIdx = idx;
+          }
         }}
       >
         {@html RubyString.toHtml(RubyString.fromToken(token))}
@@ -29,13 +43,15 @@
     display: inline-block;
     margin: 2px;
   }
-  .token.selected a {
+  .token.selected:not(.invalid) a {
     color: blue;
   }
   .token a {
     text-decoration: none;
+    color: black;
+  }
+  .token:not(.invalid) a {
     cursor: pointer;
     border-bottom: 1px solid black;
-    color: black;
   }
 </style>
