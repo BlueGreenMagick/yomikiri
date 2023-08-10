320ba665ef7b656b88de1b8084280d9133399d68
Yoonchae Lee <bluegreenmagick@gmail.com>
2023-08-10 23:13:08 +0900

    develop: remove unused fields from unidic lex to reduce size

--- unidic/unidic-build/src/transform.rs
+++ unidic/unidic-build/src/transform.rs
@@ -1,19 +1,47 @@
+use csv;
 use std::error::Error;
 use std::fs;
 use std::path::Path;
 
 type TResult<T> = core::result::Result<T, Box<dyn Error>>;
 
-/// TODO...
 pub fn transform(input_dir: &Path, transform_dir: &Path) -> TResult<()> {
     let entries = fs::read_dir(input_dir)?;
     for entry in entries {
         let entry = entry?;
         let copy_from = entry.path();
         if let Some(file_name) = copy_from.file_name() {
-            let copy_to = transform_dir.join(file_name);
-            fs::copy(&copy_from, &copy_to)?;
+            let copy_to = transform_dir.join(&file_name);
+            let file_name = file_name.to_str().unwrap();
+            match file_name {
+                "lex_naist.csv" => {
+                    transform_lex(&copy_from, &copy_to)?;
+                }
+                _ => {
+                    fs::copy(&copy_from, &copy_to)?;
+                }
+            }
         }
+    }
+    Ok(())
+}
+
+/// remove fields that are not used.
+fn transform_lex(lex_path: &Path, output_path: &Path) -> TResult<()> {
+    let mut reader = csv::Reader::from_path(lex_path)?;
+    let mut writer = csv::Writer::from_path(output_path)?;
+    for record in reader.records() {
+        let record = record?;
+        let key = record.get(0).unwrap();
+        let lid = record.get(1).unwrap();
+        let rid = record.get(2).unwrap();
+        let cost = record.get(3).unwrap();
+        let pos = record.get(4).unwrap();
+        let pos2 = record.get(5).unwrap();
+        let reading = record.get(24).unwrap();
+        let base = record.get(14).unwrap();
+        // retain only useful fields
+        writer.write_record(&[key, lid, rid, cost, pos, pos2, reading, base])?;
     }
     Ok(())
 }
