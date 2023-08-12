ea2e96edc12df1f90377ed9d2fd9556c7345a77b
Yoonchae Lee <bluegreenmagick@gmail.com>
2023-08-12 20:49:30 +0900

    fix: don't scan inside yomikiri tooltip 'yomikiri' class is set after content.js is loaded

--- extension/src/content/index.ts
+++ extension/src/content/index.ts
@@ -12,44 +12,50 @@ declare global {
   }
 }
 
-// set in ./tooltip.ts for tooltip
-// don't scan inside yomikiri tooltips
-if (!document.documentElement.classList.contains("yomikiri")) {
-  const scanner = new Scanner();
+const scanner = new Scanner();
 
-  /** Return false if not triggered on japanese text */
-  async function _trigger(x: number, y: number): Promise<boolean> {
-    const result = await scanner.scanAt(x, y);
-    if (result === null) return false;
-    console.log(result);
-    if (result.dicEntries.length === 0) {
-      highlighter.highlightRed(result.range);
-      Tooltip.hide();
-    } else {
-      highlighter.highlight(result.range);
-      await Tooltip.show(result.dicEntries, result, x, y);
-    }
-    return true;
+/** Return false if not triggered on japanese text */
+async function _trigger(x: number, y: number): Promise<boolean> {
+  const result = await scanner.scanAt(x, y);
+  if (result === null) return false;
+  console.log(result);
+  if (result.dicEntries.length === 0) {
+    highlighter.highlightRed(result.range);
+    Tooltip.hide();
+  } else {
+    highlighter.highlight(result.range);
+    await Tooltip.show(result.dicEntries, result, x, y);
+  }
+  return true;
+}
+
+const trigger = Utils.SingleQueued(_trigger);
+
+document.addEventListener("mousemove", async (ev) => {
+  // inside yomikiri tooltip
+  if (document.documentElement.classList.contains("yomikiri")) {
+    return;
   }
 
-  const trigger = Utils.SingleQueued(_trigger);
+  if (ev.shiftKey) {
+    await trigger(ev.clientX, ev.clientY);
+  }
+});
 
-  document.addEventListener("mousemove", async (ev) => {
-    if (ev.shiftKey) {
-      await trigger(ev.clientX, ev.clientY);
+document.addEventListener("click", async (ev: MouseEvent) => {
+  // inside yomikiri tooltip
+  if (document.documentElement.classList.contains("yomikiri")) {
+    return;
+  }
+
+  if (Api.isTouchScreen) {
+    const triggered = await trigger(ev.clientX, ev.clientY);
+    if (triggered === false) {
+      Tooltip.hide();
+      highlighter.unhighlight();
     }
-  });
+  }
+});
 
-  document.addEventListener("click", async (ev: MouseEvent) => {
-    if (Api.isTouchScreen) {
-      const triggered = await trigger(ev.clientX, ev.clientY);
-      if (triggered === false) {
-        Tooltip.hide();
-        highlighter.unhighlight();
-      }
-    }
-  });
-
-  window.scanner = scanner;
-  window.Api = Api;
-}
+window.scanner = scanner;
+window.Api = Api;
