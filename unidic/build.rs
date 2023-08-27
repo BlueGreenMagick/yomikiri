bf05af28e291fcd01cffe8ac0762aa7ce88331a2
Yoonchae Lee <bluegreenmagick@gmail.com>
2023-08-27 18:25:51 +0900

    develop: download unidic initially during build

--- unidic/build.rs
+++ unidic/build.rs
@@ -3,11 +3,13 @@ use filetime::FileTime;
 use std::env;
 use std::error::Error;
 use std::fs;
+use std::fs::File;
 use std::path::Path;
 use std::path::PathBuf;
 use std::time::SystemTime;
 use walkdir::{DirEntry, WalkDir};
 use yomikiri_unidic_build::{build_unidic, transform_unidic};
+use zip::ZipArchive;
 
 fn main() -> Result<(), Box<dyn Error>> {
     println!("cargo:rerun-if-changed=build.rs");
@@ -17,13 +19,26 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     // We also compare mtime because build script is rerun when compilation target changes
     let crate_dir = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap());
-    let input_dir = crate_dir.join("original");
+    let original_dir = crate_dir.join("original");
     let transform_dir = crate_dir.join("transformed");
     let output_dir = crate_dir.join("output");
     let src_dir = crate_dir.join("src");
     let build_crate_dir = crate_dir.join("unidic-build");
 
-    let input_mtime = get_last_mtime_of_dir(&input_dir)?;
+    // download original unidic if 'original' dir does not exist or is empty
+    if !original_dir.is_dir()
+        || WalkDir::new(&original_dir)
+            .into_iter()
+            .filter_entry(|e| !skip_file_entry(e))
+            .skip(1) // first entry is the directory
+            .next()
+            .is_none()
+    {
+        let _ = std::fs::create_dir(&original_dir);
+        download_unidic_original(&original_dir)?;
+    }
+
+    let input_mtime = get_last_mtime_of_dir(&original_dir)?;
     let transform_mtime = get_last_mtime_of_dir(&transform_dir)?;
     let src_mtime = get_last_mtime_of_dir(&src_dir)?;
     let build_crate_mtime = get_last_mtime_of_dir(&build_crate_dir)?;
@@ -39,7 +54,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     {
         return Ok(());
     }
-    transform_unidic(&input_dir, &transform_dir)?;
+    transform_unidic(&original_dir, &transform_dir)?;
     set_file_mtime(&transform_dir, FileTime::now())?;
 
     // re-run build only if transform has updated after last build
@@ -89,4 +104,29 @@ fn skip_file_entry(entry: &DirEntry) -> bool {
         return true;
     }
     return false;
+}
+
+/// download and unzip unidic file into `output_path`.
+/// `output_path` must exist and be a directory
+fn download_unidic_original(output_path: &Path) -> Result<(), Box<dyn Error>> {
+    let download_url = "https://clrd.ninjal.ac.jp/unidic_archive/cwj/2.2.0/unidic-cwj-2.2.0.zip";
+    let resp = ureq::get(download_url).call()?;
+    let mut tmpfile = tempfile::tempfile()?;
+    std::io::copy(&mut resp.into_reader(), &mut tmpfile)?;
+
+    // unzip files
+    let mut archive = ZipArchive::new(tmpfile)?;
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(i)?;
+        let file_name = file
+            .enclosed_name()
+            .map(|p| if p.is_dir() { None } else { p.file_name() })
+            .flatten();
+        if let Some(file_name) = file_name {
+            let dest_path = output_path.join(file_name);
+            let mut dest = File::create(&dest_path)?;
+            std::io::copy(&mut file, &mut dest)?;
+        }
+    }
+    Ok(())
 }
