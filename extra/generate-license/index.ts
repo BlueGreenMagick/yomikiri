f3cb6f3b1a6a23b9425ef8b8e8c8e97a5e7f5b47
Yoonchae Lee <bluegreenmagick@gmail.com>
2024-02-24 15:53:21 +0900

    dev: manual.toml is used for manual edits to THIRD_PARTY.txt

--- extra/generate-license/index.ts
+++ extra/generate-license/index.ts
@@ -3,6 +3,8 @@ import process from "node:process";
 import os from "node:os";
 import path from "node:path";
 import which from "which";
+import toml from "toml";
+import fs from "node:fs";
 
 function getShell(): string {
   if (process.platform == "win32") {
@@ -36,7 +38,7 @@ function formatRustDisclaimer(licensesJSON: string): string {
   const licenses = JSON.parse(licensesJSON);
   let disclaimers: string[] = [];
   for (const pack of licenses.third_party_libraries) {
-    let disclaimer = `The following software may be included in this product: ${pack.package_name} (${pack.package_version}) with license (${pack.license}). This software contains the following license and notice below:`;
+    let disclaimer = `The following software may be included in this product: ${pack.package_name} (${pack.package_version}) (license: ${pack.license}). This software contains the following license and notice below:`;
     disclaimer += "\n\n";
     for (const license of pack.licenses) {
       disclaimer += license.text;
@@ -68,8 +70,38 @@ async function generateYarnLicenses() {
   return stdout as string;
 }
 
+interface TOMLResource {
+  name: string;
+  url?: string;
+  license: string;
+  text: string;
+  text_html?: boolean;
+}
+
+interface ManualTomlFile {
+  res: TOMLResource[];
+}
+
+function manualEdits() {
+  const manualTomlPath = path.join(__dirname, "res", "manual.toml");
+  const manualToml = fs.readFileSync(manualTomlPath, {
+    encoding: "utf-8",
+  });
+  const resources = toml.parse(manualToml) as ManualTomlFile;
+
+  let disclaimers: string[] = [];
+  for (const res of resources.res) {
+    let disclaimer = `The following software may be included in this product: ${res.name} (license: ${res.license}). This software contains the following license and notice below:`;
+    disclaimer += "\n\n";
+    disclaimer += res.text;
+    disclaimers.push(disclaimer);
+  }
+  return disclaimers.join("\n");
+}
+
 async function main() {
-  let disclaimer = await generateYarnLicenses();
+  let disclaimer = manualEdits();
+  disclaimer += await generateYarnLicenses();
   disclaimer += "\n-------------------------\n";
   let rustOutput = await runRustCommand();
   disclaimer += formatRustDisclaimer(rustOutput);
