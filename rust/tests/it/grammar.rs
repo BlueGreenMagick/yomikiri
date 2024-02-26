953255768d9a3867143eb800b18e5de7effc43c8
Yoonchae Lee <bluegreenmagick@gmail.com>
2024-02-27 00:17:25 +0900

    dev: fix grammar test syntax

--- rust/tests/it/grammar.rs
+++ rust/tests/it/grammar.rs
@@ -9,38 +9,38 @@ type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 fn test_grammar(
     backend: &mut SharedBackend<File>,
     sentence: &'static str,
-    rules: &[&'static str],
+    name: &str,
+    short: &str,
 ) -> Result<()> {
-    let char_at = sentence
-        .chars()
-        .position(|c| c == '＿')
-        .unwrap_or_else(|| panic!("'＿' not found in sentence: {}", &sentence));
+    let char_at = sentence.chars().position(|c| c == '＿').unwrap_or(0);
     let sentence = sentence.replace('＿', "");
     let result = backend.tokenize(&sentence, char_at)?;
-    let grammars: Vec<String> = result.grammars.iter().map(|g| g.name.to_string()).collect();
 
-    for rule in rules {
-        if !grammars.iter().any(|g| g == rule) {
-            panic!(
-                "Testing sentence: {}\nCould not find grammar {} in [{}]",
-                sentence,
-                rule,
-                grammars.join(",")
-            );
-        }
+    let grammars: Vec<(String, String)> = result
+        .grammars
+        .iter()
+        .map(|g| (g.name.to_string(), g.short.to_string()))
+        .collect();
+    if !grammars
+        .iter()
+        .any(|(n, s)| (n.as_str(), s.as_str()) == (name, short))
+    {
+        panic!(
+            "Testing sentence: {}\nCould not find grammar ({}, {}) in [{:?}]",
+            sentence, name, short, grammars
+        );
     }
     Ok(())
 }
 
 macro_rules! test {
-    ($($test:ident : $(| $sentence:literal $($name:literal)+)+)+) => {
+    ($($test:ident: $name:literal $short:literal $(| $sentence:literal)+)+) => {
       $(
         #[test]
         fn $test() -> Result<()> {
           let mut backend = setup_backend();
           $(
-            let rules = vec![$($name),+];
-            test_grammar(&mut backend, $sentence, &rules)?;
+            test_grammar(&mut backend, $sentence, $name, $short)?;
           )+
           Ok(())
         }
@@ -49,17 +49,18 @@ macro_rules! test {
 }
 
 test!(
-  られる:
-    | "私は蜂に＿刺された。" "ーられる" "ーた"
-    | "タバコを＿吸われた。" "ーられる" "ーた"
-    | "このお酒は芋から＿作られている" "ーられる"
-    | "私が＿閉ざされた" "ーられる" "ーた"
-    | "彼は＿刺されませんでした" "ーられる" "ーた"
 
-  た:
-  | "本を＿買った。" "ーた"
-  | "日本語が上手に＿なったね。" "ーた"
-  | "フグを＿食べましたか。" "ーた"
-  | "彼が＿死んだ。" "ーた"
-  | "＿泳いだ。" "ーた"
+  られる: "ーられる" "passive suffix"
+    | "私は蜂に＿刺された。"
+    | "タバコを＿吸われた。"
+    | "このお酒は芋から＿作られている"
+    | "私が＿閉ざされた"
+    | "彼は＿刺されませんでした"
+
+  た: "ーた" "past tense"
+  | "本を＿買った。"
+  | "日本語が上手に＿なったね。"
+  | "フグを＿食べましたか。"
+  | "彼が＿死んだ。"
+  | "泳いだ。"
 );
