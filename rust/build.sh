9fb63fb099ee9cfd9e004fb86e4cf80db348bee5
Yoonchae Lee <bluegreenmagick@gmail.com>
2024-03-03 01:09:48 +0900

    dev: build for ios only if the target triple is installed

--- rust/build.sh
+++ rust/build.sh
@@ -14,8 +14,33 @@ cargo run --manifest-path "../unidic/Cargo.toml"
 cargo build --release --lib
 cargo run --features "uniffi/cli uniffi-bindgen" --bin uniffi-bindgen generate --language swift --lib-file ../target/release/libyomikiri_rs.a --out-dir ./swift ./src/uniffi_yomikiri.udl
 
-cargo build --release --lib --target "aarch64-apple-ios"
-cargo build --release --lib --target "aarch64-apple-ios-sim"
+# build for ios and ios-simulator if required targets are installed
+
+# check if rust target is installed
+# https://users.rust-lang.org/t/determining-installed-target-list-when-rustup-is-not-in-use/100594/2
+target_installed () {
+  target="$1"
+  sysroot="$(rustc --print sysroot)"
+  if [ -d "$sysroot/lib/rustlib/$target" ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+IOS_TARGET="aarch64-apple-ios"
+IOS_SIM_TARGET="aarch64-apple-ios-sim"
+
+if target_installed $IOS_TARGET; then
+  cargo build --release --lib --target $IOS_TARGET
+else
+  echo "Skipped building for ios because target 'aarch64-apple-ios' is not installed for rust"
+fi
+if target_installed $IOS_SIM_TARGET; then
+  cargo build --release --lib --target $IOS_SIM_TARGET
+else
+  echo echo "Skipped building for ios simulator because target 'aarch64-apple-ios-sim' is not installed for rust"
+fi
 
 # build wasm
 wasm-pack build --scope yomikiri --target web
