d9c3988668fdd97ea2b79c0f945e3e1fbcb7ad58
Yoonchae Lee <bluegreenmagick@gmail.com>
2024-03-05 22:30:32 +0900

    dev: change unidic pos short value from number to char

--- unidic-types/src/lib.rs
+++ unidic-types/src/lib.rs
@@ -1,4 +1,7 @@
+// For unidic pos2 value 'ＡＡ'
 #![allow(uncommon_codepoints)]
+// make sure all unidic pos has unique short value
+#![deny(unreachable_patterns)]
 
 #[derive(Debug)]
 pub struct UnknownValueError {
@@ -14,12 +17,6 @@ impl UnknownValueError {
 }
 
 type Result<T> = std::result::Result<T, UnknownValueError>;
-
-/// valid num: 0 - 61
-#[inline(always)]
-fn short_value(num: usize) -> u8 {
-    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".as_bytes()[num]
-}
 
 macro_rules! unidic_pos_enum {
     ($( $pos_name:ident $($pos2_name:ident)? ),+) => {
@@ -60,9 +57,9 @@ macro_rules! unidic_pos2_from_unidic {
 macro_rules! unidic_pos {
     (
         $(
-            $pos_name:ident $pos_value:ident $($pos_num: literal)?
+            $pos_name:ident $pos_value:ident $($pos_short: literal)?
             $(= $pos2_name:ident
-                $(- $pos2_value:ident $pos2_num:literal)+
+                $(- $pos2_value:ident $pos2_short:literal)+
             )?
         )+
     ) => {
@@ -89,11 +86,11 @@ macro_rules! unidic_pos {
                 match &self {
                     $(
                         $(
-                            UnidicPos::$pos_name => short_value($pos_num),
+                            UnidicPos::$pos_name => $pos_short as u8,
                         )?
                         $(
                             $(
-                                UnidicPos::$pos_name($pos2_name::$pos2_value) => short_value($pos2_num),
+                                UnidicPos::$pos_name($pos2_name::$pos2_value) => $pos2_short as u8,
                             )+
                         )?
 
@@ -102,14 +99,14 @@ macro_rules! unidic_pos {
             }
 
             pub fn from_short(short: u8) -> Result<Self> {
-                match short {
+                match short as char {
                     $(
                       $(
-                        $pos_num => Ok(UnidicPos::$pos_name),
+                        $pos_short => Ok(UnidicPos::$pos_name),
                       )?
                       $(
                         $(
-                            $pos2_num => Ok(UnidicPos::$pos_name($pos2_name::$pos2_value)),
+                            $pos2_short => Ok(UnidicPos::$pos_name($pos2_name::$pos2_value)),
                         )+
                       )?
                     )+
@@ -124,68 +121,68 @@ macro_rules! unidic_pos {
 unidic_pos!(
     Noun 名詞
     = UnidicNounPos2
-        - 助動詞語幹 1
-        - 固有名詞 2
-        - 普通名詞 3
-        - 数詞 4
+        - 助動詞語幹 'a'
+        - 固有名詞 'b'
+        - 普通名詞 'c'
+        - 数詞 'd'
 
     Particle 助詞
     = UnidicParticlePos2
-        - 係助詞 5
-        - 格助詞 6
-        - 接続助詞 7
-        - 準体助詞 8
-        - 終助詞 9
-        - 副助詞 10
+        - 係助詞 'e'
+        - 格助詞 'f'
+        - 接続助詞 'g'
+        - 準体助詞 'h'
+        - 終助詞 'i'
+        - 副助詞 'j'
 
     Verb 動詞
     = UnidicVerbPos2
-        - 一般 11
-        - 非自立可能 12
+        - 一般 'k'
+        - 非自立可能 'l'
 
     Adjective 形容詞
     = UnidicAdjectivePos2
-        - 一般 13
-        - 非自立可能 14
+        - 一般 'm'
+        - 非自立可能 'n'
 
     NaAdjective 形状詞
     = UnidicNaAdjectivePos2
-        - 一般 15
-        - 助動詞語幹 16
-        - タリ 17
+        - 一般 'o'
+        - 助動詞語幹 'p'
+        - タリ 'q'
 
     Interjection 感動詞
     = UnidicInterjectionPos2
-        - 一般 18
-        - フィラー 19
+        - 一般 'r'
+        - フィラー 's'
 
     Suffix 接尾辞
     = UnidicSuffixPos2
-        - 名詞的 20
-        - 形容詞的 21
-        - 動詞的 22
-        - 形状詞的 23
+        - 名詞的 't'
+        - 形容詞的 'u'
+        - 動詞的 'v'
+        - 形状詞的 'w'
 
-    AuxVerb 助動詞 24
-    Whitespace 空白 25
-    Pronoun 代名詞 26
+    AuxVerb 助動詞 'x'
+    Whitespace 空白 'y'
+    Pronoun 代名詞 'z'
 
     Symbol 記号
     = UnidicSymbolPos2
-        - 一般 27
-        - 文字 28
+        - 一般 'A'
+        - 文字 'B'
 
     SupplementarySymbol 補助記号
     = UnidicSupplementarySymbolPos2
-        - 読点 29
-        - 一般 30
-        - 括弧開 31
-        - 括弧閉 32
-        - ＡＡ 33
-        - 句点 34
+        - 読点 'C'
+        - 一般 'D'
+        - 括弧開 'E'
+        - 括弧閉 'F'
+        - ＡＡ 'G'
+        - 句点 'H'
 
-    Conjunction 接続詞 35
-    Prefix 接頭辞 36
-    PrenounAdjectival 連体詞 37
-    Adverb 副詞 38
+    Conjunction 接続詞 'I'
+    Prefix 接頭辞 'J'
+    PrenounAdjectival 連体詞 'K'
+    Adverb 副詞 'L'
 );
