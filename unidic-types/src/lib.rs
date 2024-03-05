637b821eecd11380205e31d8b5e66ff2e9d197e0
Yoonchae Lee <bluegreenmagick@gmail.com>
2024-03-05 23:03:56 +0900

    dev: implement Error trait for unidic-types UnknownValueError

--- unidic-types/src/lib.rs
+++ unidic-types/src/lib.rs
@@ -3,10 +3,21 @@
 // make sure all unidic pos has unique short value
 #![deny(unreachable_patterns)]
 
+use std::error::Error;
+use std::fmt::Display;
+
 #[derive(Debug)]
 pub struct UnknownValueError {
     pub encountered: String,
 }
+
+impl Display for UnknownValueError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.encountered.fmt(f)
+    }
+}
+
+impl Error for UnknownValueError {}
 
 impl UnknownValueError {
     pub fn new<S: Into<String>>(value: S) -> Self {
