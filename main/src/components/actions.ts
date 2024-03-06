c0b568027f17f5da67a09fc2c47751d73bbed9e0
Yoonchae Lee <bluegreenmagick@gmail.com>
2024-03-06 23:20:17 +0900

    fix: platform class not attached to iframe tooltip but top document

--- main/src/components/actions.ts
+++ main/src/components/actions.ts
@@ -4,8 +4,8 @@ import { Platform } from "@platform";
  * adds one of .desktop, .ios, .iosapp depending on platform to <html>
  * Should be attached to all root svelte element
  */
-export function platformClass(_: HTMLElement) {
-  const node = document.documentElement;
+export function platformClass(attached: HTMLElement) {
+  const node = attached.ownerDocument.documentElement;
   if (!node) return;
   if (Platform.IS_DESKTOP) {
     node.classList.add("desktop");
