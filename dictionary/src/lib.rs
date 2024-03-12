a13fa7419b8c465418effa4c9064fc09a887bcff
Yoonchae Lee <bluegreenmagick@gmail.com>
2024-03-12 15:25:25 +0900

    refactor: expose structs in dictionary crate as global namespace

--- dictionary/src/lib.rs
+++ dictionary/src/lib.rs
@@ -1,3 +1,7 @@
 pub mod entry;
 pub mod error;
 pub mod file;
+
+pub use entry::{Entry, Form, PartOfSpeech, Reading, Sense};
+pub use error::{Error, Result};
+pub use file::{DictEntryIndex, DictTermIndex};
