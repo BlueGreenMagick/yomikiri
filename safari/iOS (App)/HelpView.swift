c7742421263bbb8e6116cff06598d97ddcedf2fb
Yoonchae Lee <bluegreenmagick@gmail.com>
2024-03-18 20:18:14 +0900

    feat(ios): switch help image every 3 seconds

--- safari/iOS (App)/HelpView.swift
+++ safari/iOS (App)/HelpView.swift
@@ -12,54 +12,72 @@ struct HelpView: View {
 
     var body: some View {
         VStack(alignment: .center, spacing: 20) {
-            Picker("Table of Contents", selection: $viewModel.sectionLabel) {
-                ForEach(ViewModel.sections, id: \.self) { section in
-                    Text(section.title)
+            Picker("Table of Contents", selection: $viewModel.currentSectionIndex) {
+                ForEach(0 ..< ViewModel.sections.count, id: \.self) { idx in
+                    Text(ViewModel.sections[idx].title).tag(idx)
                 }
             }
-            Image("help-1-1-1")
+            Image(viewModel.imageName)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .border(.gray)
                 .padding(10)
             Text(viewModel.description)
         }.padding(20)
+            .task {
+                Task {
+                    repeat {
+                        try? await Task.sleep(nanoseconds: 3 * 1000 * 1000 * 1000)
+                        viewModel.nextImage()
+                    } while !Task.isCancelled
+                }
+            }
     }
 
-    class ViewModel: ObservableObject {
+    final class ViewModel: ObservableObject {
         static let sections: [Section] = [
             Section(title: "Main App", items: [
-                Item(description: "Type in Japanese text", image: "help-1-1-1"),
+                Item(description: "Type in Japanese text", images: ["help-1-1-1", "help-1-1-2"]),
             ]),
             Section(title: "Safari Extension", items: [
-                Item(description: "Select aA in address bar", image: ""),
+                Item(description: "Select aA in address bar", images: [""]),
             ]),
             Section(title: "Anki", items: [
-                Item(description: "Go to Settings tab", image: ""),
+                Item(description: "Go to Settings tab", images: [""]),
             ]),
         ]
 
-        @Published var sectionLabel: String
-        @Published var description: String
-        @Published var imageName: String
-        var currentSectionIndex: Int
-        var currentItemIndex: Int
+        @Published var currentSectionIndex: Int
+        @Published var currentItemIndex: Int
+        @Published var currentImageIndex: Int
+
+        var currentItem: Item {
+            ViewModel.sections[currentSectionIndex].items[currentItemIndex]
+        }
+
+        var sectionTitle: String {
+            ViewModel.sections[currentSectionIndex].title
+        }
+
+        var description: String {
+            currentItem.description
+        }
+
+        var imageName: String {
+            currentItem.images[currentImageIndex]
+        }
 
         init() {
             currentSectionIndex = 0
             currentItemIndex = 0
-            sectionLabel = ""
-            description = ""
-            imageName = ""
-            updateDisplay()
+            currentImageIndex = 0
         }
 
-        func updateDisplay() {
-            let section = ViewModel.sections[currentSectionIndex]
-            let item = section.items[currentItemIndex]
-            sectionLabel = section.title
-            description = item.description
-            imageName = item.image
+        func nextImage() {
+            currentImageIndex += 1
+            if currentImageIndex >= currentItem.images.count {
+                currentImageIndex = 0
+            }
         }
     }
 }
@@ -71,7 +89,7 @@ struct Section: Hashable {
 
 struct Item: Hashable {
     var description: String
-    var image: String
+    var images: [String]
 }
 
 #Preview {
