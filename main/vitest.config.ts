c84a022c7c16e86e423cf366bdef3e72da197b4f
Yoonchae Lee <bluegreenmagick@gmail.com>
2024-04-12 18:47:42 +0900

    dev: fix vitest config ts

--- main/vitest.config.ts
+++ main/vitest.config.ts
@@ -1,8 +1,54 @@
 import { defineConfig } from 'vitest/config'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import sveltePreprocess from "svelte-preprocess";
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import path from "node:path";
+import postCssImport from "postcss-import";
+import Package from "./package.json" assert { type: "json" };
+
+const svelteConfiguredPlugin = svelte({
+  preprocess: sveltePreprocess({
+    postcss: {
+      plugins: [postCssImport()]
+    }
+  }),
+  compilerOptions: { css: true },
+  onwarn: (warning, defaultHandler) => {
+    const ignore = [
+      "a11y-no-noninteractive-tabindex",
+      "a11y-click-events-have-key-events",
+      "css-unused-selector",
+    ];
+    if (ignore.includes(warning.code)) return;
+    defaultHandler!(warning)
+  },
+  extensions: ['.svelte', '.svg']
+})
+
+
+/* resolve path relative to './src' */
+function fullpath(relative: string): string {
+  return path.resolve(import.meta.dirname, 'src', relative)
+}
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  plugins: [svelteConfiguredPlugin],
+  define: {
+    __APP_VERSION__: `"${Package.version}"`,
+    __APP_PLATFORM__: '"chrome"',
+  },
+  assetsInclude: ['**/*.wasm', '**/*.json.gz', '**/*.yomikiridict', '**/*.yomikiriindex', '**/*.chunk'],
+  resolve: {
+    alias: {
+      '@platform': fullpath('platform/desktop'),
+      '~': fullpath('.'),
+      '@icons': fullpath('../node_modules/ionicons/dist/svg'),
+    }
+  },
+  build: {
+    target: [
+      "es2017", "chrome99", "firefox55", "safari14.1"
+    ],
+  },
   test: {
     environment: 'jsdom'
   }
