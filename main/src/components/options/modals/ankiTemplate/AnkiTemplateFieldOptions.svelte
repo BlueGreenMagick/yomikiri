7088af99198f8ca14f6e0a7687b5d92b11b39a6d
Yoonchae Lee <bluegreenmagick@gmail.com>
2024-05-20 12:09:46 +0900

    feat: anki template field options descriptions are made detailed

--- main/src/components/options/modals/ankiTemplate/AnkiTemplateFieldOptions.svelte
+++ main/src/components/options/modals/ankiTemplate/AnkiTemplateFieldOptions.svelte
@@ -9,19 +9,23 @@
   {#if template.type === "word"}
     <OptionSelect
       bind:selected={template.options.form}
-      options={["as-is", "dict-form", "main-dict-form"]}
+      options={[
+        ["as-is", "Form used in sentence"],
+        ["dict-form", "Dictionary conjugation form (ーる）"],
+        ["main-dict-form", "Main form used in dictionary"],
+      ]}
       title="Form"
     >
-      Word styling
+      Which form of word to use.
     </OptionSelect>
   {/if}
   {#if template.type === "sentence"}
     <OptionSelect
       bind:selected={template.options.word}
       options={["none", "cloze", "bold", "span"]}
-      title="Style selected word"
+      title="Selected Word Style"
     >
-      Word styling
+      Style selected word within sentence
     </OptionSelect>
   {/if}
   {#if template.type === "word" || template.type === "sentence"}
@@ -29,7 +33,13 @@
       bind:selected={template.options.style}
       options={["basic", "furigana-anki", "furigana-html", "kana-only"]}
       title="Style"
-    />
+    >
+      Whether to use furigana, or convert word to kana.<br />
+      'furigana-anki' outputs
+      <a href="https://docs.ankiweb.net/templates/fields.html#ruby-characters"
+        >Anki-format furigana</a
+      >.
+    </OptionSelect>
   {/if}
   {#if template.type === "" || template.type === "translated-sentence" || template.type === "url" || template.type === "link"}
     <div class="gray">No configurable options</div>
