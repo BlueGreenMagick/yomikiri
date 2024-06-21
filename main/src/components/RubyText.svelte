70ea3408d0988485dfb28558aa1e26101df61a31
Yoonchae Lee <bluegreenmagick@gmail.com>
2024-06-21 19:15:26 +0900

    fix: re-create ruby text potentiall fix a bug where tooltip readings won't change

--- main/src/components/RubyText.svelte
+++ main/src/components/RubyText.svelte
@@ -4,10 +4,12 @@
   export let text: RubyString;
 </script>
 
-{#each text as unit}
-  {#if unit.ruby === undefined}
-    {unit.base}
-  {:else}
-    <ruby>{unit.base}<rt data-rt={unit.ruby} /></ruby>
-  {/if}
-{/each}
+{#key text}
+  {#each text as unit}
+    {#if unit.ruby === undefined}
+      {unit.base}
+    {:else}
+      <ruby>{unit.base}<rt data-rt={unit.ruby} /></ruby>
+    {/if}
+  {/each}
+{/key}
