4ef384660f96dfdacade9e4d9771305b85d9de98
Yoonchae Lee <bluegreenmagick@gmail.com>
2024-08-10 16:00:55 +0900

    add stack to ts YomikiriError

--- main/src/lib/error.ts
+++ main/src/lib/error.ts
@@ -5,10 +5,12 @@ export class YomikiriError {
   readonly message: string;
   /** contains message as first element, and leaf error message as last. */
   readonly details: string[];
+  readonly stack: string[];
 
-  constructor(message: string, details?: string[]) {
+  constructor(message: string, stack: string[], details?: string[]) {
     this.message = message;
     this.details = details ?? [message];
+    this.stack = stack ?? "";
   }
 
   /**
@@ -18,10 +20,6 @@ export class YomikiriError {
   static from(err: unknown): YomikiriError {
     if (err instanceof YomikiriError) return err;
 
-    if (err === undefined || err === null) {
-      return new YomikiriError("Undefined error");
-    }
-
     if (err instanceof Error) {
       // create details from chain of causes
       if ((err as Error & { cause: unknown }).cause !== undefined) {
@@ -29,28 +27,42 @@ export class YomikiriError {
           (err as Error & { cause: unknown }).cause,
         ).context(err.message);
       } else {
-        return new YomikiriError(err.message);
+        const stack = formatStack(err.stack ?? "");
+        return new YomikiriError(err.message, stack, []);
       }
     }
 
+    const tempErr = new Error();
+    const currStack = formatStack(tempErr.stack ?? "");
+
+    if (err === undefined || err === null) {
+      const err = new YomikiriError("Undefined error", currStack);
+      return err;
+    }
+
     if (typeof err === "string") {
-      return new YomikiriError(err);
+      return new YomikiriError(err, currStack);
     }
 
     console.error(err);
     try {
       // eslint-disable-next-line
       const msg = err.toString();
-      return new YomikiriError(`Unknown error: ${msg}`);
+      return new YomikiriError(`Unknown error: ${msg}`, currStack);
     } catch {
       return new YomikiriError(
         "Unknown error. Check the browser console for more details.",
+        currStack,
       );
     }
   }
 
   context(message: string): YomikiriError {
     const details = [message, ...this.details];
-    return new YomikiriError(message, details);
+    return new YomikiriError(message, this.stack, details);
   }
+}
+
+function formatStack(stack: string): string[] {
+  return stack.split("\n").map((line) => line.trim());
 }
