d78515900572dfed146bee874f5efbc17897dbf5
Yoonchae Lee <bluegreenmagick@gmail.com>
2024-08-13 11:38:19 +0900

    dev: fix prettier config not working with vscode extension

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,5 @@
-import prettierSvlete from "prettier-plugin-svelte";
-
 /** @type {import('prettier').Config} */
 export default {
   experimentalTernaries: true,
-  plugins: [prettierSvlete],
+  plugins: ["prettier-plugin-svelte"],
 };
