b22284a70bbc2ae9a1a14511b5af6e4aa677303d
Yoonchae Lee <bluegreenmagick@gmail.com>
2024-08-16 11:56:48 +0900

    refactor: combine ffi module into one file

--- crates/yomikiri-backend/src/ffi.rs
+++ crates/yomikiri-backend/src/ffi.rs
@@ -1,6 +1,9 @@
-use super::backend::RustBackend;
+use crate::dictionary::Dictionary;
 use crate::error::{FFIResult, ToUniFFIResult};
-use anyhow::Result;
+use crate::tokenize::{create_tokenizer, RawTokenizeResult};
+use crate::{utils, SharedBackend};
+
+use anyhow::{Context, Result};
 use flate2::read::GzDecoder;
 use tempfile::NamedTempFile;
 use yomikiri_dictionary::dictionary::DictionaryView;
@@ -11,7 +14,65 @@ use yomikiri_dictionary::{DICT_FILENAME, DICT_METADATA_FILENAME};
 use std::fs;
 use std::io::Write;
 use std::path::{Path, PathBuf};
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
+
+#[derive(uniffi::Object)]
+pub struct RustBackend {
+    inner: Mutex<SharedBackend<Vec<u8>>>,
+}
+
+impl RustBackend {
+    pub(crate) fn try_from_paths<P: AsRef<Path>>(dict_path: P) -> Result<Arc<RustBackend>> {
+        utils::setup_logger();
+        let tokenizer = create_tokenizer();
+        let dictionary = Dictionary::from_paths(dict_path)?;
+        let inner = SharedBackend {
+            tokenizer,
+            dictionary,
+        };
+        let inner = Mutex::new(inner);
+        let backend = RustBackend { inner };
+        Ok(Arc::new(backend))
+    }
+}
+
+#[uniffi::export]
+impl RustBackend {
+    #[uniffi::constructor]
+    pub fn new(dict_path: String) -> FFIResult<Arc<RustBackend>> {
+        Self::_new(dict_path).uniffi()
+    }
+
+    pub fn tokenize(&self, sentence: String, char_at: u32) -> FFIResult<RawTokenizeResult> {
+        self._tokenize(sentence, char_at).uniffi()
+    }
+
+    pub fn search(&self, term: String, char_at: u32) -> FFIResult<RawTokenizeResult> {
+        self._search(term, char_at).uniffi()
+    }
+}
+
+impl RustBackend {
+    fn _new(dict_path: String) -> Result<Arc<RustBackend>> {
+        Self::try_from_paths(&dict_path)
+    }
+
+    fn _tokenize(&self, sentence: String, char_at: u32) -> Result<RawTokenizeResult> {
+        let mut backend = self.inner.lock().unwrap();
+        let char_at = usize::try_from(char_at)
+            .with_context(|| format!("Failed to convert char_at '{}' to usize", char_at))?;
+        let result = backend.tokenize(&sentence, char_at)?;
+        Ok(result)
+    }
+
+    fn _search(&self, term: String, char_at: u32) -> Result<RawTokenizeResult> {
+        let mut backend = self.inner.lock().unwrap();
+        let char_at = usize::try_from(char_at)
+            .with_context(|| format!("Failed to convert char_at '{}' to usize", char_at))?;
+        let result = backend.search(&term, char_at)?;
+        Ok(result)
+    }
+}
 
 #[derive(uniffi::Object)]
 pub struct DictFilesReplaceJob {
--- crates/yomikiri-backend/src/ffi/mod.rs
+++
@@ -1,2 +0,0 @@
-mod backend;
-mod update_dict;
