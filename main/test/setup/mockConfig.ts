b470cf1037823e2874f9342e1ee59a981674f2d6
Yoonchae Lee <bluegreenmagick@gmail.com>
2024-08-23 20:39:29 +0900

    test: implement config subscription in tests

--- main/test/setup/mockConfig.ts
+++ main/test/setup/mockConfig.ts
@@ -1,18 +1,28 @@
 import { CONFIG_VERSION, type StoredConfiguration } from "lib/config";
 import { DesktopPlatform } from "platform/desktop";
 
+type ConfigSubscriber = (config: StoredConfiguration) => void;
+
 /* Mock config */
 let storedConfig: StoredConfiguration = {
   version: "0.0.1",
   config_version: CONFIG_VERSION,
 };
 
+const subscribers: ConfigSubscriber[] = [];
+
 DesktopPlatform.getConfig = () => {
   return Promise.resolve(storedConfig);
 };
 DesktopPlatform.saveConfig = (conf) => {
   storedConfig = conf;
+  for (const subscriber of subscribers) {
+    subscriber(storedConfig);
+  }
   return Promise.resolve();
+};
+DesktopPlatform.subscribeConfig = (subscriber: ConfigSubscriber) => {
+  subscribers.push(subscriber);
 };
 
 export function setStoredConfig(conf: StoredConfiguration) {
