3116f4b6f4ac53183191d25e14b2f5977d0531e0
Yoonchae Lee <bluegreenmagick@gmail.com>
2024-09-02 20:43:13 +0900

    use camelCase for jm entity enum serialization

--- crates/jmdict/src/utils.rs
+++ crates/jmdict/src/utils.rs
@@ -32,7 +32,7 @@ macro_rules! jm_entity_enum {
   ) => {
       #[cfg_attr(feature = "wasm", derive(Tsify))]
       #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
-      #[serde(rename_all = "lowercase")]
+      #[serde(rename_all = "camelCase")]
       $( #[$enum_attrs] )*
       pub enum $enum_name {
           $(
