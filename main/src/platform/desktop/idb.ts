9e4ceba8ec2feead2d3c83e716c01206f2859830
Yoonchae Lee <bluegreenmagick@gmail.com>
2024-09-16 12:09:22 +0900

    refactor: Delete unused db and object stores on idb upgrade

--- main/src/platform/desktop/idb.ts
+++ main/src/platform/desktop/idb.ts
@@ -1,21 +1,34 @@
-import { openDB, type DBSchema, type IDBPDatabase } from "idb";
+import { deleteDB, openDB, type DBSchema, type IDBPDatabase } from "idb";
 
-export type FileName = "yomikiri-dictionary";
+const DB_NAME = "yomikiri";
+const DB_FILES_STORE = "files";
+const FILENAMES = ["yomikiri-dictionary"] as const;
 
-interface FilesDBSchema extends DBSchema {
+export type FileName = (typeof FILENAMES)[number];
+
+interface YomikiriDBSchema extends DBSchema {
   files: {
     key: FileName;
     value: Uint8Array;
   };
 }
 
-export type FilesDB = IDBPDatabase<FilesDBSchema>;
-
-async function openFilesDB(): Promise<IDBPDatabase<FilesDBSchema>> {
-  return await openDB<FilesDBSchema>("files", 1, {
+async function openFilesDB(): Promise<IDBPDatabase<YomikiriDBSchema>> {
+  return await openDB<YomikiriDBSchema>(DB_NAME, 1, {
     upgrade(db) {
-      db.createObjectStore("files", { keyPath: "key" });
-      // nothing here yet
+      let has_files_store = false;
+      for (const name of db.objectStoreNames) {
+        if (name === DB_FILES_STORE) {
+          has_files_store = true;
+        } else {
+          db.deleteObjectStore(name);
+        }
+      }
+      if (!has_files_store) {
+        db.createObjectStore(DB_FILES_STORE);
+      }
+
+      void deleteUnusedDBs();
     },
   });
 }
@@ -24,20 +37,23 @@ export async function idbReadFile(
   file: FileName,
 ): Promise<Uint8Array | undefined> {
   const db = await openFilesDB();
-  return db.get("files", file);
+  const files = db.get(DB_FILES_STORE, file);
+  db.close();
+  return files;
 }
 
 export async function idbReadFiles(
   files: FileName[],
 ): Promise<[FileName, Uint8Array | undefined][]> {
   const db = await openFilesDB();
-  const tx = db.transaction("files", "readonly");
+  const tx = db.transaction(DB_FILES_STORE, "readonly");
   const results: [FileName, Uint8Array | undefined][] = [];
   for (const file of files) {
     const content = await tx.store.get(file);
     results.push([file, content]);
   }
   await tx.done;
+  db.close();
   return results;
 }
 
@@ -45,18 +61,30 @@ export async function idbWriteFiles(
   files: [FileName, Uint8Array][],
 ): Promise<void> {
   const db = await openFilesDB();
-  const tx = db.transaction("files", "readwrite");
+  const tx = db.transaction(DB_FILES_STORE, "readwrite");
   for (const [file, content] of files) {
     await tx.store.put(content, file);
   }
   await tx.done;
+  db.close();
 }
 
 export async function deleteFiles(filenames: FileName[]): Promise<void> {
   const db = await openFilesDB();
-  const tx = db.transaction("files", "readwrite");
+  const tx = db.transaction(DB_FILES_STORE, "readwrite");
   for (const file of filenames) {
     await tx.store.delete(file);
   }
+  db.close();
   await tx.done;
+}
+
+/** Deletes unused idb databases */
+async function deleteUnusedDBs(): Promise<void> {
+  const dbs = await indexedDB.databases();
+  for (const db of dbs) {
+    if (db.name !== undefined && db.name !== DB_NAME) {
+      await deleteDB(db.name);
+    }
+  }
 }
