e7470e3408848a86c59c2525d198f312d77fdb87
Yoonchae Lee <bluegreenmagick@gmail.com>
2025-03-02 22:45:02 +0900

    build(android): setup ankidroid api dependency

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.anki.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
