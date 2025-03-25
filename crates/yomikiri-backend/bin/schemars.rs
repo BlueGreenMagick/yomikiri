327d2f3f804e511d920477fdfad795f1b82b7e7f
Yoonchae Lee <bluegreenmagick@gmail.com>
2025-03-25 23:22:54 +0900

    build: generate backend-binding to /generated/backend-binding

--- crates/yomikiri-backend/bin/schemars.rs
+++ crates/yomikiri-backend/bin/schemars.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::fs;
 use std::path::Path;
 
@@ -5,8 +6,16 @@ use schemars::schema_for;
 use yomikiri_rs::run::RunArgAndReturnTypes;
 
 fn main() {
+    let args: Vec<_> = env::args().collect();
+    if args.len() < 2 {
+        panic!("The output path must be provided as argument");
+    }
+    let output_path = Path::new(&args[1]);
+    if let Some(parent) = output_path.parent() {
+        fs::create_dir_all(parent).unwrap();
+    }
+
     let schema = schema_for!(RunArgAndReturnTypes);
-    let path = Path::new("./bindings/generated.schema.json");
     let schema_string = serde_json::to_string_pretty(&schema).unwrap();
-    fs::write(&path, &schema_string).unwrap();
+    fs::write(&output_path, &schema_string).unwrap();
 }
