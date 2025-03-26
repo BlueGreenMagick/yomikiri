55b7fd0f900a93df6abc3ae6bd4cfec3fa744994
Yoonchae Lee <bluegreenmagick@gmail.com>
2025-03-26 23:37:47 +0900

    feat(android): configure webview settings

--- android/app/src/main/java/com/yoonchae/yomikiri/YomikiriWebView.kt
+++ android/app/src/main/java/com/yoonchae/yomikiri/YomikiriWebView.kt
@@ -1,6 +1,10 @@
 package com.yoonchae.yomikiri
 
+import android.annotation.SuppressLint
 import android.view.ViewGroup
+import android.webkit.CookieManager
+import android.webkit.WebResourceRequest
+import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,15 +16,42 @@ import androidx.compose.ui.viewinterop.AndroidView
 fun YomikiriWebView(modifier: Modifier = Modifier) {
     AndroidView(factory={
         WebView(it).apply {
-            this.webViewClient = YomikiriWebViewClient()
+            val webView = this
+
             layoutParams = ViewGroup.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.MATCH_PARENT,
             )
+
+            settings.apply {
+                useWideViewPort = true
+                @SuppressLint("SetJavaScriptEnabled")
+                javaScriptEnabled = true
+                allowFileAccess = true
+                allowContentAccess = true
+                loadWithOverviewMode = true
+                builtInZoomControls = true
+                displayZoomControls = false
+                setSupportZoom(true)
+                domStorageEnabled = true
+                cacheMode = WebSettings.LOAD_DEFAULT
+                mediaPlaybackRequiresUserGesture = false
+                setGeolocationEnabled(true)
+                offscreenPreRaster = true // should only be true if this webview is visible
+            }
+
+            CookieManager.getInstance().apply {
+                setAcceptCookie(true)
+                setAcceptThirdPartyCookies(webView, true)
+            }
+
+            webViewClient = object : WebViewClient() {
+                override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                    return false
+                }
+            }
         }
     }, update={
         it.loadUrl("https://www.google.com")
     }, modifier=modifier.fillMaxSize())
 }
-
-class YomikiriWebViewClient: WebViewClient() {}
