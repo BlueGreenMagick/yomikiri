a5cd4dd795dd1d35279d523dbb529640abc6430a
Yoonchae Lee <bluegreenmagick@gmail.com>
2025-03-26 22:47:24 +0900

    fix(android): fix Webview height to fill

--- android/app/src/main/java/com/yoonchae/yomikiri/YomikiriWebView.kt
+++ android/app/src/main/java/com/yoonchae/yomikiri/YomikiriWebView.kt
@@ -1,7 +1,9 @@
 package com.yoonchae.yomikiri
 
+import android.view.ViewGroup
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
@@ -11,10 +13,14 @@ fun YomikiriWebView(modifier: Modifier = Modifier) {
     AndroidView(factory={
         WebView(it).apply {
             this.webViewClient = YomikiriWebViewClient()
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT,
+            )
         }
     }, update={
         it.loadUrl("https://www.google.com")
-    })
+    }, modifier=modifier.fillMaxSize())
 }
 
 class YomikiriWebViewClient: WebViewClient() {}
