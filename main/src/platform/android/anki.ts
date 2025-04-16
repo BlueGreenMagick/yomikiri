53338a6d32a8dbee0aef587a90603b42fdb1e579
Yoonchae Lee <bluegreenmagick@gmail.com>
2025-04-16 22:35:16 +0900

    feat(android): anki.ts implemented

---
+++ main/src/platform/android/anki.ts
@@ -0,0 +1,43 @@
+import type { AnkiNote } from "@/lib/anki";
+import type { AnkiInfo } from "./anki";
+import type { IAnkiAddNotes } from "./anki";
+import type { IAnkiOptions } from "./anki";
+
+export * from "../common/anki";
+
+export namespace AndroidAnkiApi {
+  export const IS_DESKTOP = false;
+  export const IS_IOS = false;
+  export const IS_IOSAPP = false;
+  export const IS_ANDROID = true;
+
+  export function setAnkiInfo(_ankiInfoJson: string): void {
+    throw new Error("Unimplemented");
+  }
+
+  export async function requestAnkiInfo(): Promise<void> {
+    await Promise.resolve();
+    throw new Error("Unimplemented");
+  }
+
+  export async function getAnkiInfo(): Promise<AnkiInfo> {
+    await Promise.resolve();
+    throw new Error("Unimplemented");
+  }
+
+  export async function checkConnection(): Promise<void> {
+    await Promise.resolve();
+    throw new Error("Unimplemented");
+  }
+
+  export async function addNote(_note: AnkiNote): Promise<boolean> {
+    await Promise.resolve();
+    throw new Error("Unimplemented");
+  }
+}
+
+AndroidAnkiApi satisfies IAnkiAddNotes;
+AndroidAnkiApi satisfies IAnkiOptions;
+
+export type AndroidAnkiApi = typeof AndroidAnkiApi;
+export const AnkiApi = AndroidAnkiApi;
