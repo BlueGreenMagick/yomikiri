780a5eab45d0d7762241424b495c91109da66dd0
Yoonchae Lee <bluegreenmagick@gmail.com>
2025-04-16 21:46:49 +0900

    feat(android): AndroidPlatform and messaging is semi-implemented

---
+++ main/src/platform/android/index.ts
@@ -0,0 +1,73 @@
+import {
+  migrateConfigObject,
+  type StoredCompatConfiguration,
+} from "@/lib/compat";
+import type {
+  IPlatform,
+  TranslateResult,
+  TTSRequest,
+  TTSVoice,
+  VersionInfo,
+} from "../common";
+import type { StoredConfiguration } from "@/lib/config";
+import { getTranslation } from "#platform/translate";
+
+export namespace AndroidPlatform {
+  export const IS_DESKTOP = false;
+  export const IS_IOS = false;
+  export const IS_IOSAPP = false;
+  export const IS_ANDROID = true;
+
+  /** TODO */
+  export async function getConfig(): Promise<StoredCompatConfiguration> {
+    await Promise.resolve();
+    return {};
+  }
+
+  /** TODO */
+  export function subscribeConfig(
+    _subscriber: (config: StoredConfiguration) => void,
+  ): void {}
+
+  /** TODO */
+  export async function migrateConfig(): Promise<StoredConfiguration> {
+    await Promise.resolve();
+    return migrateConfigObject({});
+  }
+
+  export async function saveConfig(_config: StoredConfiguration) {
+    await Promise.resolve();
+    throw new Error("Unimplemented");
+  }
+
+  export function openOptionsPage(): void {
+    throw new Error("Unimplemented");
+  }
+
+  export async function versionInfo(): Promise<VersionInfo> {
+    await Promise.resolve();
+    throw new Error("Unimplemented");
+  }
+
+  export async function japaneseTTSVoices(): Promise<TTSVoice[]> {
+    await Promise.resolve();
+    throw new Error("Unimplemented");
+  }
+
+  export async function playTTS(_req: TTSRequest): Promise<void> {
+    await Promise.resolve();
+    throw new Error("Unimplemented");
+  }
+
+  export async function translate(text: string): Promise<TranslateResult> {
+    return getTranslation(text);
+  }
+
+  export function openExternalLink(_url: string): void {
+    throw new Error("Unimplemented");
+  }
+}
+
+AndroidPlatform satisfies IPlatform;
+export const Platform = AndroidPlatform;
+export const PagePlatform = AndroidPlatform;
---
+++ main/src/platform/android/messaging.ts
@@ -0,0 +1,95 @@
+import { YomikiriError } from "@/lib/error";
+import { createPromise } from "@/lib/utils";
+import type { RunMessageMap } from "../shared/backend";
+
+/** Secret key used in android message handler */
+declare const __ANDROID_MESSAGE_SECRET_KEY: string;
+
+declare global {
+  interface Window {
+    __yomikiriInterface?: AndroidMessagingInterface;
+  }
+}
+
+type PostMessageFn = (message: string) => void;
+type OnReceiveMessageFn = (message: string) => void;
+interface AndroidMessagingInterface {
+  postMessage: PostMessageFn;
+  onmessage?: OnReceiveMessageFn;
+}
+
+let messagingInterface: AndroidMessagingInterface = {
+  postMessage: () => {
+    throw new Error("Could not capture js interface");
+  },
+};
+
+if (Object.prototype.hasOwnProperty.call(window, "__yomikiriInterface")) {
+  messagingInterface = window.__yomikiriInterface!;
+  delete window["__yomikiriInterface"];
+}
+
+export interface AndroidMessageMap extends RunMessageMap {}
+
+export type MessageRequest<K extends keyof AndroidMessageMap> =
+  AndroidMessageMap[K][0];
+export type MessageResponse<K extends keyof AndroidMessageMap> =
+  AndroidMessageMap[K][1];
+
+type JSONString = string;
+
+// response format for extension and ios messaging
+export interface SuccessfulResponseMessage {
+  id: number;
+  success: true;
+  resp: JSONString;
+}
+
+export interface FailedResponseMessage {
+  id: number;
+  success: false;
+  error: JSONString;
+}
+
+export type ResponseMessage = SuccessfulResponseMessage | FailedResponseMessage;
+
+const responseHandlers = new Map<
+  number,
+  [(response: unknown) => void, (error: YomikiriError) => void]
+>();
+
+messagingInterface.onmessage = (message) => {
+  const response = JSON.parse(message) as ResponseMessage;
+  if (!responseHandlers.has(response.id)) {
+    console.error(
+      `No message handler exists for id: ${response.id}, but received response:`,
+      response,
+    );
+  }
+  const [onSuccess, onError] = responseHandlers.get(response.id)!;
+  if (response.success) {
+    onSuccess(JSON.parse(response.resp));
+  } else {
+    onError(YomikiriError.from(JSON.parse(response.error)));
+  }
+};
+
+/** Unique id attached to each message. Used to map response to request. */
+let messageId = 0;
+
+export async function sendMessage<K extends keyof AndroidMessageMap>(
+  key: K,
+  request: MessageRequest<K>,
+): Promise<MessageResponse<K>> {
+  messageId += 1;
+  const message = {
+    id: messageId,
+    key,
+    request: JSON.stringify(request),
+  };
+  const stringifiedMessage = JSON.stringify(message);
+  messagingInterface.postMessage(stringifiedMessage);
+  const [promise, resolve, reject] = createPromise<MessageResponse<K>>();
+  responseHandlers.set(messageId, [resolve as (resp: unknown) => void, reject]);
+  return promise;
+}
