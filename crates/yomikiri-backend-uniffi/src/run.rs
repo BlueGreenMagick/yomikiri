3419f0d1c3ce581140269f0dee09d326f23eb41e
Yoonchae Lee <bluegreenmagick@gmail.com>
2025-08-20 21:52:00 +0900

    add methods

--- crates/yomikiri-backend-uniffi/src/run.rs
+++ crates/yomikiri-backend-uniffi/src/run.rs
@@ -1,7 +1,8 @@
+use anyhow::Result;
 use schemars::JsonSchema;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
-use crate::error::FFIResult;
+use crate::error::{FFIResult, ToUniFFIResult};
 use crate::RustBackend;
 
 #[derive(Debug, Serialize, JsonSchema)]
@@ -9,26 +10,42 @@ pub struct RunArgAndReturnTypes {
     example: String,
 }
 
-#[derive(Debug, Serialize, JsonSchema)]
+#[derive(Debug, Serialize, Deserialize, JsonSchema)]
 pub enum RunAppCommand {
     StartMigration(StartMigrationArgs),
 }
 
 #[derive(Debug, Serialize, JsonSchema)]
+pub enum RunAppReturnValues {
+    StartMigration(StartMigrationReturn),
+}
+
+#[derive(Debug, Serialize, Deserialize, JsonSchema)]
 pub struct StartMigrationArgs {}
+
+pub type StartMigrationReturn = ();
 
 #[uniffi::export]
 impl RustBackend {
-    pub fn runApp(&self, command: String) -> FFIResult<String> {}
+    pub fn run_app(&self, command: String) -> FFIResult<String> {
+        self._run_app(&command).uniffi()
+    }
 }
 
 impl RustBackend {
-    pub fn _runApp(&self, command: &str) -> Result<String> {
+    pub fn _run_app(&self, command: &str) -> Result<String> {
+        use RunAppCommand::*;
+
         let cmd: RunAppCommand = serde_json::from_str(command)?;
-        match cmd {
-            StartMigration(args) => self._runStartMigration(args),
-        }
+
+        let json = match cmd {
+            StartMigration(args) => serde_json::to_string(&self._run_start_migration(args)?)?,
+        };
+
+        Ok(json)
     }
 
-    pub fn _runStartMigration(args: StartMigrationArgs) -> Result<String> {}
+    pub fn _run_start_migration(&self, args: StartMigrationArgs) -> Result<StartMigrationReturn> {
+        unimplemented!()
+    }
 }
